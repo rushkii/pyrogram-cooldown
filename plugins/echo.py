3055081cedec0a516c5a1f15f8df32ed2e1d7741
Kee <47179236+rushkii@users.noreply.github.com>
2021-07-20 10:36:56 +0700

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -4,6 +4,5 @@ from .utils import cooldown
 bot = Client
 
 @bot.on_message(filters.command(['test']) & cooldown.wait(5))
-@cooldown.wait(5)
 async def echo_text(_,msg):
     await msg.reply(msg.text)
